5e07f74fb5a7103eee6381b379e01e0e7143ad36
Travis K <traviskean04@live.com>
2015-07-20 14:10:37 -0400

    Harmonic Sum Program

---
+++ Harmonic_Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿//Travis Kean
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmonic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Harmonic Sum from left to right
+            double sum = 0; //initialize sum to 0
+            for(int x = 1; x <= 50000; x++) 
+            {
+                sum = sum + (1.0 / x); //will sum each number together from 1/1 to 1/50000
+            }
+
+            Console.WriteLine("The harmonic sum from n=1 to n= 50,000, left to right is " + sum); //displays sum
+
+            sum = 0;
+            for(int x = 50000; x >= 1; x--)
+            {
+                sum = sum + (1.0 / x); //will sum each number together from 1/50000 to 1/1 
+            }
+
+            Console.WriteLine("The harmonic sum from n = 1 to n = 50,000, right to left is " + sum); //displays sum
+
+            Console.ReadLine();
+        }
+    }
+}
